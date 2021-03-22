0c9ba826bd2e002f16a4bb80c31cb0e1d9528178
Sabirzyanov <57670787+Sabirzyanov@users.noreply.github.com>
2021-03-22 20:58:14 +0300

    Update excel_parser.py

---
+++ data/excel_parser.py
@@ -0,0 +1,36 @@
+import openpyxl
+
+wb = openpyxl.load_workbook("rasp.xlsx")
+sheet_names = wb.sheetnames
+
+
+def parseLesson(sym1, sym2):
+    temp_lessons = []
+    return_dict = {}
+    a = 8
+    lesson_num = 1
+    sheet = wb[sheet_names[0]]
+    while a <= 41:
+        for i in range(a, a + 6):
+            for cellObj in sheet[f"{sym1}{i}:{sym2}{i}"]:
+                lesson = ""
+                for cell in cellObj:
+                    if cell.value is None:
+                        break
+                    lesson += f"{str(cell.value)} "
+                temp_lessons.append(f"{str(lesson_num)} - {lesson}")
+            a += 1
+            lesson_num += 1
+        lesson_num = 1
+
+    temp_var = 6
+    while temp_var <= 36:
+        temp = temp_lessons[temp_var - 6:temp_var]
+        return_dict[temp_var // 5 - 1] = "".join(temp)
+        temp_var += 6
+
+    return return_dict
+
+
+programmers_lessons = parseLesson("R", "T")
+print(programmers_lessons)
