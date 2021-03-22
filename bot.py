55e40e6524ff72b7d2ca340f928fa4889477258f
WingiM <wingimobile@gmail.com>
2021-03-22 19:41:21 +0300

    Bot base done

--- bot.py
+++ bot.py
@@ -1,15 +1,75 @@
 import os
-from telegram.ext import Updater
+import datetime
+import sqlite3
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram.ext import Updater, CommandHandler, ConversationHandler, MessageHandler, Filters
 from dotenv import load_dotenv
 
-
 load_dotenv('.env')
+
+
+def start(update, context):
+    update.message.reply_text(f'Здравствуй, {update.message.from_user["username"]}!')
+
+
+def daily_timetable_start(update, context):
+    markup = [['Назад']]
+    key = ReplyKeyboardMarkup(markup, resize_keyboard=True, one_time_keyboard=True)
+    update.message.reply_text('Укажите номер группы и дату в формате NNN ДД ММ ГГГГ', reply_markup=key)
+    return 1
+
+
+def get_daily_timetable(update, context):
+    try:
+        message = update.message.text
+        if message == 'Назад':
+            update.message.reply_text('Возвращаемся назад', reply_markup=ReplyKeyboardRemove())
+            return ConversationHandler.END
+        message = message.split()
+        if len(message) != 4:
+            raise KeyError
+        group, day, month, year = map(int, message)
+        date = datetime.date(year, month, day)
+        days_delta = (date - datetime.date.today()).days
+        if days_delta < 0:
+            update.message.reply_text('Не могу вернуться в прошлое', reply_markup=ReplyKeyboardRemove())
+            return ConversationHandler.END
+        if days_delta > 7:
+            update.message.reply_text('Дальше недели не вижу', reply_markup=ReplyKeyboardRemove())
+            return ConversationHandler.END
+        weekday = date.weekday()
+        if weekday == 6:
+            update.message.reply_text('В воскресенье не учимся', reply_markup=ReplyKeyboardRemove())
+            return ConversationHandler.END
+        connection = sqlite3.connect('db/timetables.db')
+        cursor = connection.cursor()
+        schedule = cursor.execute("""SELECT schedule FROM default_timetables_students 
+                                    WHERE "group" = ? AND weekday = ?""", (group, weekday)).fetchone()
+        update.message.reply_text(schedule, reply_markup=ReplyKeyboardRemove())
+        return ConversationHandler.END
+
+    except (KeyError, ValueError):
+        update.message.reply_text('Некорректный запрос', reply_markup=ReplyKeyboardRemove())
+        return ConversationHandler.END
+
+
+def leave(update, context):
+    update.message.reply_text('Ок')
+    return ConversationHandler.END
 
 
 def main():
     updater = Updater(os.getenv('BOT_TOKEN'), use_context=True)
 
     dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(ConversationHandler(
+        entry_points=[CommandHandler('day', daily_timetable_start)],
+        states={
+            1: [MessageHandler(Filters.text, get_daily_timetable)]
+        },
+        fallbacks=[CommandHandler('exit', leave)]
+    ))
 
     updater.start_polling()
     updater.idle()
@@ -17,5 +77,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
--- data/db_dispatcher.py
+++ data/db_dispatcher.py
@@ -1,4 +1,39 @@
 import sqlite3
+import json
 
 connection = sqlite3.connect('../db/timetables.db')
-cursor = connection.cursor()
+
+
+def set_default_timetables(file):
+    """Устанавливает стандартные расписания по дням недели"""
+    file = json.load(file)
+    groups = file['groups']
+    teachers = file['teachers']
+    cur = connection.cursor()
+
+    for group, weekdays in groups.items():
+        for weekday, schedule in weekdays.items():
+            if not cur.execute("""SELECT * FROM default_timetables_students WHERE "group" = ? AND weekday = ?""",
+                               (group, weekday)).fetchall():
+                cur.execute("""INSERT INTO default_timetables_students("group", weekday, schedule) VALUES (?, ?, ?)""",
+                            (group, weekday, schedule))
+                continue
+            cur.execute("""UPDATE default_timetables_students SET schedule = ? WHERE weekday = ? AND "group" = ?""",
+                        (schedule, weekday, group))
+
+    for teacher, weekdays in teachers.items():
+        for weekday, schedule in weekdays.items():
+            if not cur.execute("""SELECT * FROM default_timetables_teachers WHERE weekday = ? AND teacher_name = ?""",
+                               (weekday, teacher)).fetchall():
+                cur.execute(
+                    """INSERT INTO default_timetables_teachers(teacher_name, weekday, schedule) VALUES (?, ?, ?)""",
+                    (teacher, weekday, schedule))
+                continue
+            cur.execute("""UPDATE default_timetables_teachers SET schedule = ? WHERE teacher_name = ? AND weekday = ?""",
+                        (schedule, teacher, weekday))
+
+    connection.commit()
+
+
+set_default_timetables(open('../test.json', encoding='utf8'))
+
