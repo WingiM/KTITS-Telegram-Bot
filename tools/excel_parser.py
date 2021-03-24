84c2262875edbfa9d38d4968c5db624ad1d018f9
Sabirzyanov <57670787+Sabirzyanov@users.noreply.github.com>
2021-03-24 19:36:47 +0300

    Update excel_parser.py

--- tools/excel_parser.py
+++ tools/excel_parser.py
@@ -1,36 +1,51 @@
 import openpyxl
 
-wb = openpyxl.load_workbook("../workbooks/rasp1.xlsx")
+wb = openpyxl.load_workbook("rasp.xlsx")
 sheet_names = wb.sheetnames
 
 
-def parse_lesson(start_col, end_col):
-    temp_lessons = []
-    return_dict = {}
-    a = 8
+def parse(start_col="C", end_col="E", sheet_number=0):
+    one_sheet_lessons = {}
+    start_col_ord, end_col_ord = ord(start_col), ord(end_col)
+    group_number = 100
     lesson_num = 1
-    sheet = wb[sheet_names[0]]
-    while a <= 41:
-        for i in range(a, a + 6):
-            for cell_obj in sheet[f"{start_col}{i}:{end_col}{i}"]:
-                lesson = ""
-                for cell in cell_obj:
-                    if cell.value is None:
-                        break
-                    lesson += f"{str(cell.value)} "
-                temp_lessons.append(f"{str(lesson_num)} - {lesson}")
-            a += 1
-            lesson_num += 1
-        lesson_num = 1
+    sheet = wb[sheet_names[sheet_number]]
+    while start_col_ord <= ord("R"):
+        return_dict = {}
+        temp_lessons = []
+        a = 8 if sheet_number == 0 else 7
+        while a <= 43:
+            for i in range(a, a + 6):
+                for cell_obj in sheet[f"{chr(start_col_ord)}{i}:{chr(end_col_ord)}{i}"]:
+                    lesson = ""
+                    for cell in cell_obj:
+                        if cell.value is None:
+                            break
+                        lesson += f"{str(cell.value)} "
+                    temp_lessons.append(f"{str(lesson_num)} - {lesson.replace('_', '')}")
+                a += 1
+                lesson_num += 1
+            lesson_num = 1
+        temp_var = 6
+        while temp_var <= 36:
+            temp = temp_lessons[temp_var - 6:temp_var]
+            return_dict[temp_var // 5 - 1] = "".join(temp)
+            temp_var += 6
+        one_sheet_lessons[group_number] = return_dict
+        group_number += 1
 
-    temp_var = 6
-    while temp_var <= 36:
-        temp = temp_lessons[temp_var - 6:temp_var]
-        return_dict[temp_var // 5 - 1] = "".join(temp)
-        temp_var += 6
+        start_col_ord += 3
+        end_col_ord += 3
 
-    return return_dict
+    return one_sheet_lessons
 
 
-programmers_lessons = parse_lesson("R", "T")
-print(programmers_lessons)
+def main():
+    all_lessons = {}
+    for i in range(len(sheet_names)):
+        all_lessons[i] = parse("C", "E", i)
+    return all_lessons
+
+
+if __name__ == '__main__':
+    print(main())
