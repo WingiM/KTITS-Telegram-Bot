e87e90e5723296b8259d92c3217292b0bfd30d1e
WingiM <wingimobile@gmail.com>
2021-04-01 17:29:27 +0300

    Improved image sending for admins

--- tools/admin_bot.py
+++ tools/admin_bot.py
@@ -1,8 +1,7 @@
 import os
 import sqlite3
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Bot, Update, ChatPhoto
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Bot, Update
 from telegram.ext import Updater, CommandHandler, ConversationHandler, MessageHandler, Filters
-
 
 bot = Bot(os.getenv("BOT_TOKEN"))
 
@@ -13,11 +12,11 @@ def start(update, context):
         update.message.reply_text('Для привязки аккаунта используйте /link')
 
 
-def link_checker(update, context):
+def link_checker(update, _):
     connection = sqlite3.connect('db/timetables.db')
     cursor = connection.cursor()
-    admins = cursor.execute("""SELECT * FROM users WHERE chat_id = ?""", (update.message.from_user['id'], )).fetchall()
-    if not cursor.execute("""SELECT chat_id FROM admin_users WHERE chat_id = ?""", (update.message.from_user["id"], )).fetchone():
+    if not cursor.execute("""SELECT chat_id FROM admin_users WHERE chat_id = ?""",
+                          (update.message.from_user["id"],)).fetchone():
         return False
     return True
 
@@ -41,9 +40,10 @@ def linker(update, context):
         return 1
     connection = sqlite3.connect('db/timetables.db')
     cursor = connection.cursor()
-    cursor.execute("""INSERT INTO admin_users VALUES(?)""", (update.message.from_user['id'], ))
+    cursor.execute("""INSERT INTO admin_users VALUES(?)""", (update.message.from_user['id'],))
     connection.commit()
-    update.message.reply_text('Успешно привязали аккаунт.\nИспользуйте /send для отправки сообщений группам', reply_markup=ReplyKeyboardRemove())
+    update.message.reply_text('Успешно привязали аккаунт.\nИспользуйте /send для отправки сообщений группам',
+                              reply_markup=ReplyKeyboardRemove())
     return ConversationHandler.END
 
 
@@ -53,8 +53,9 @@ def message_send(update, context):
         return ConversationHandler.END
     markup = [['Выйти']]
     key = ReplyKeyboardMarkup(markup, resize_keyboard=True, one_time_keyboard=True)
-    update.message.reply_text("Введите номер группы или несколько групп(через пробел), которой вы хотите отпраить сообщение\n"
-                              "Например: 120 или 120 102 130", reply_markup=key)
+    update.message.reply_text(
+        "Пожалуйста, введите номера групп, которым хотите отправить сообщение\n"
+        "Например: 120 121 220", reply_markup=key)
     return 1
 
 
@@ -63,10 +64,9 @@ def select_group(update, context):
     if message.lower() == 'выйти':
         return leave(update, context)
     groups = set(message.split(" "))
-    for i in groups:
-        if not check_group(i):
-            update.message.reply_text("Вы ввели неправильный номер группы")
-            return 1
+    for group in groups:
+        if not check_group(group):
+            groups.remove(group)
     context.user_data['to_group'] = groups
     markup = [['Выйти']]
     key = ReplyKeyboardMarkup(markup, resize_keyboard=True, one_time_keyboard=True)
@@ -75,25 +75,29 @@ def select_group(update, context):
 
 
 def message_to_group(update: Update, context):
-    file = update.message.photo[-1]
-    newFile = Bot(os.getenv("ADMIN_BOT_TOKEN")).get_file(file_id=file.file_id)
-    newFile.download(custom_path="image_temp/file.png")
-    update.message.reply_photo(file)
-    message = update.message.text
-    if message is not None:
-        if message.lower() == 'выйти':
-            return leave(update, context)
+    message = update.message.text if update.message.text else ''
+    photo_passed = False
+    if message and message.lower() == 'выйти':
+        return leave(update, context)
+    try:
+        print(len(update.message.photo))
+        file = update.message.photo[-1]
+        newFile = Bot(os.getenv("ADMIN_BOT_TOKEN")).get_file(file_id=file.file_id)
+        newFile.download(custom_path="image_temp/file.png")
+        photo_passed = True
+    except IndexError:
+        pass
     current_groups = context.user_data['to_group']
     connection = sqlite3.connect('db/timetables.db')
     cursor = connection.cursor()
     for group in current_groups:
-        users = cursor.execute("""SELECT chat_id FROM users WHERE "group" = ? """, (group, )).fetchall()
-        for i in users:
-            if message is not None:
-                bot.sendMessage(chat_id=i[0], text="Учебная часть:\n" + message)
-            if file is not None:
-                bot.sendMessage(chat_id=i[0], text="Учебная часть:\n")
-                bot.sendPhoto(chat_id=i[0], photo=open("image_temp/file.png", 'rb'))
+        users = cursor.execute("""SELECT chat_id FROM users WHERE "group" = ? """, (group,)).fetchall()
+        for user in users:
+            if photo_passed:
+                bot.sendPhoto(chat_id=user[0], photo=open("image_temp/file.png", 'rb'),
+                              caption=("Учебная часть:\n" + update.message.caption) if update.message.caption else None)
+            else:
+                bot.sendMessage(chat_id=user[0], text="Учебная часть:\n" + message)
         update.message.reply_text(f'Успешно отправили сообщение группе {group}', reply_markup=ReplyKeyboardRemove())
     return ConversationHandler.END
 
@@ -106,7 +110,7 @@ def check_group(group):
     return False
 
 
-def leave(update, context):
+def leave(update, _):
     update.message.reply_text('Хорошо, отменяем', reply_markup=ReplyKeyboardRemove())
     return ConversationHandler.END
 
